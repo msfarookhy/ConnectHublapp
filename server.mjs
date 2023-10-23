634297f38f163fae05516f0a060b6b6f27866525
Misbah Farookhy <msfarookhy@gmail.com>
2023-10-24 00:25:15 +0530

    deployment

--- server.mjs
+++ server.mjs
@@ -19,9 +19,11 @@ app.listen(port, () => {
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path")
-  app.use(express.static(path.resolve(__dirname,'frontend','build')))
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname,'frontend','build', 'index.html'))
+  
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'))
   });
   
 
